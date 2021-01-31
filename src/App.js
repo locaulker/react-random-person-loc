e2f7fe7013bca31eec5c05c6affd318db57d2c99
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-31 00:34:21 -0500

    Random users' setup completed

--- src/App.js
+++ src/App.js
@@ -8,9 +8,77 @@ import {
   FaLock,
 } from 'react-icons/fa'
 const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+const defaultImage = 'https://randomuser.me/api/portraits/women/44.jpg'
+
 function App() {
-  return <h2>random user starter</h2>
+  const [loading, setLoading] = useState(true)
+  const [person, setPerson] = useState(null)
+  const [title, setTitle] = useState('name')
+  const [value, setValue] = useState('random person')
+
+  const handleValue = e => {
+    console.log(e.target)
+  }
+
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={(person && person.image) || defaultImage}
+            alt='Random User'
+            className='user-img'
+          />
+          <p className='user-title'>My {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button
+              className='icon'
+              data-label='name'
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className='btn' type='button'>
+            {loading ? 'Loading...' : 'Random User'}
+          </button>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
