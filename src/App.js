19007e4622d152c364d1e686fd34986215cc75e2
Lawrence O. Caulker <locaulker@gmail.com>
2021-01-31 02:36:30 -0500

    Implemented API data

--- src/App.js
+++ src/App.js
@@ -8,13 +8,50 @@ import {
   FaLock,
 } from 'react-icons/fa'
 const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/women/44.jpg'
+const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [person, setPerson] = useState(null)
   const [title, setTitle] = useState('name')
   const [value, setValue] = useState('random person')
+
+  const getPerson = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    const person = data.results[0]
+
+    const { phone, email } = person
+    const { large: image } = person.picture
+    const {
+      login: { password },
+    } = person
+    const { first, last } = person.name
+    const {
+      dob: { age },
+    } = person
+    const {
+      street: { number, name },
+    } = person.location
+
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    }
+    setPerson(newPerson)
+    setLoading(false)
+    setTitle('name')
+    setValue(newPerson.name)
+  }
+
+  useEffect(() => {
+    getPerson()
+  }, [])
 
   const handleValue = e => {
     console.log(e.target)
@@ -30,7 +67,7 @@ function App() {
             alt='Random User'
             className='user-img'
           />
-          <p className='user-title'>My {title} is</p>
+          <p className='user-title'>my {title} is</p>
           <p className='user-value'>{value}</p>
           <div className='values-list'>
             <button
@@ -72,8 +109,8 @@ function App() {
               <FaLock />
             </button>
           </div>
-          <button className='btn' type='button'>
-            {loading ? 'Loading...' : 'Random User'}
+          <button className='btn' type='button' onClick={getPerson}>
+            {loading ? 'Loading...' : 'Get New User'}
           </button>
         </div>
       </div>
